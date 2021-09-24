74c8762cf9235231ba711a8bc4de32a41b93da5f
Stanislav <verbatim8872@gmail.com>
2021-09-24 15:59:53 +0300

    add styles for form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,28 +2,36 @@ import React from 'react'
 import { useState } from 'react/cjs/react.development'
 
 const Form = ({ addPost, setShowModal }) => {
-    
+
     const [ formPost, setFormPost ] = useState({ title: '', description: '' })
 
     const submitHandler = (e) => {
         e.preventDefault()
-        addPost({ ...formPost, id: Date.now() })
-        setShowModal(false)
+        if(formPost.title || formPost.description){
+            addPost({ ...formPost, id: Date.now() })
+            setShowModal(false)
+        }
     }
 
     return (
-        <form>
+        <form className='form' >
+            <h2 className='title'>Add new post</h2>
             <input 
                 type='text' 
                 value={ formPost.title }
                 onChange={ (e) => setFormPost({ ...formPost, title: e.target.value }) }
+                className='formInput'
+                placeholder='Title'
             />
             <input 
                 type='text' 
                 value={ formPost.description }
                 onChange={ (e) => setFormPost({ ...formPost, description: e.target.value }) }
+                className='formInput'
+                placeholder='Description'
             />
             <button 
+                className='btn formBtn'
                 onClick={ submitHandler } 
             >Add</button> 
         </form>
